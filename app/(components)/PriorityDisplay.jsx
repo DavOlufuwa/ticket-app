c1463dd2706efca2b859bb107495bb26d8064c54
Dav.O <davolufuwa@gmail.com>
2023-11-06 01:08:37 +0100

    first commit

---
+++ app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,32 @@
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const PriorityDisplay = ({ priority }) => {
+  return (
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`${priority > 0 ? "text-red-400" : "text-gray-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`${priority > 1 ? "text-red-400" : "text-gray-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`${priority > 2 ? "text-red-400" : "text-gray-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`${priority > 3 ? "text-red-400" : "text-gray-400"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`${priority > 4 ? "text-red-400" : "text-gray-400"}`}
+      />
+    </div>
+  );
+};
+
+export default PriorityDisplay;
